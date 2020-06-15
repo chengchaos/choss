960cb9f905aad02301019b51f3e672316c731876
Cheng <c.b.cheng@accenture.com>
2020-06-15 23:51:24 +0800

    project init .

---
+++ choss-worker/src/main/java/luxe/chaos/choss/ChossApplication.java
@@ -0,0 +1,13 @@
+package luxe.chaos.choss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChossApplication.class, args);
+	}
+
+}
